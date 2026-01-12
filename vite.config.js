14872088b4e2ded38e64ff1127df7a22f2d399cf
zhilka <zhilka@zhilkas-MacBook-Pro.local>
2026-01-12 12:45:37 -0500

    config: update for custom domain zhilyakov.org

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/guess-the-animal/',
+    base: '/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
