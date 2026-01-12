61f54be7b19938b4315b003f6bbb4317b54ca48c
zhilka <zhilka@zhilkas-MacBook-Pro.local>
2026-01-12 12:24:58 -0500

    chore: configure github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // base: '/guess-the-animal/', // Uncomment this if deploying to GitHub Pages at https://<USERNAME>.github.io/guess-the-animal/
+    base: '/guess-the-animal/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
